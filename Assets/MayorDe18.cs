8fef6ab689d8c76b5f4e86748d6518d383f042dd
AryBacher <47170979@est.ort.edu.ar>
2022-04-06 15:52:56 -0300

    Initial commit

---
+++ Assets/MayorDe18.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayorDe18 : MonoBehaviour
+{
+    public int edad;
+    public Text Texto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (edad >= 18)
+        {
+            Texto.text = "Usted es mayor de edad";
+        }
+
+        else
+        {
+            Texto.text = "Usted no es mayor de edad";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
