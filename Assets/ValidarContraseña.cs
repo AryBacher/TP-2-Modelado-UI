c3f879d5cf39c73aa5dad48780b89cdd3b0bf293
arybac <aryyeze@gmail.com>
2022-04-08 16:21:38 -0300

    Finished

--- Assets/ValidarContraseña.cs
+++ Assets/ValidarContraseña.cs
@@ -1,13 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ValidarContraseña : MonoBehaviour
 {
+    string contraseñaCorrecta;
+    string contraseñaUsuario;
+    public Text textomsj;
+    public Text ingresadoUsuario;
+    public GameObject msjBox;
     // Start is called before the first frame update
     void Start()
     {
-        
+        msjBox.SetActive(false);
+        contraseñaCorrecta = "abc123";
     }
 
     // Update is called once per frame
@@ -17,6 +24,20 @@ public class ValidarContraseña : MonoBehaviour
     }
     public void ValidarContrasenia()
     {
+        contraseñaUsuario = ingresadoUsuario.text;
 
+        if (contraseñaUsuario == contraseñaCorrecta)
+        {
+            msjBox.SetActive(true);
+            textomsj.text = "Contraseña Correcta. Bienvenido!";
+            Debug.Log("Contraseña Correcta. Bienvenido!");
+        }
+
+        else
+        {
+            msjBox.SetActive(true);
+            textomsj.text = "Contraseña Incorrecta. Por favor, ingrese su contraseña correctamente";
+            Debug.Log("Contraseña Incorrecta. Por favor, ingrese su contraseña correctamente");
+        }
     }
 }
